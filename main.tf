e8b491aa59869a9de8ac2702be3f001943ebda77
Mukesh <msahu201989@gmail.com>
2022-12-09 22:32:57 +0530

    change

--- main.tf
+++ main.tf
@@ -26,5 +26,5 @@ module "vpc" {
 #}
 
 output "app_subnets" {
-  value = lookup(lookup(lookup(module.vpc, "private_subnets", null), "app", null), "subnets", null)[*].id
+  value = lookup(module.vpc, "private_subnets", null)
 }
