37802b23078d6469808d0739bfd1cfa368ca6ac0
Mukesh <msahu201989@gmail.com>
2022-12-11 21:31:12 +0530

    security greoip

--- main.tf
+++ main.tf
@@ -67,11 +67,11 @@ module "apps" {
   instance_type        = each.value.instance_type
   min_size             = each.value.min_size
   max_size             = each.value.max_size
-  vpc_id               = element([for i, j in module.vpc : j.vpc_id], 0)
+  vpc_id               = element([for i, j in module.vpc : j.vpc_id ], 0)
+  BASTION_NODE         = var.BASTION_NODE
 #  lb_listener_priority = each.value.lb_listener_priority
 #  type                 = each.value.type
 #  vpc_id               = element([for i, j in module.vpc : j.vpc_id], 0)
-  BASTION_NODE         = var.BASTION_NODE
 #  app_port_no          = each.value.app_port_no
 #  PROMETHEUS_NODE      = var.PROMETHEUS_NODE
 #  vpc_cidr             = element([for i, j in module.vpc : j.vpc_cidr], 0)
