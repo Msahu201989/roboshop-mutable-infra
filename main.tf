20fba18819ce84e380a34dc0e7ea1598628f79b2
Mukesh <msahu201989@gmail.com>
2022-12-13 21:05:55 +0530

    listners

--- main.tf
+++ main.tf
@@ -70,10 +70,8 @@ module "apps" {
   max_size             = each.value.max_size
   vpc_id               = element([for i, j in module.vpc : j.vpc_id ], 0)
   BASTION_NODE         = var.BASTION_NODE
-  app_port_no          = each.value.app_port_no
   lb_listener_priority = each.value.lb_listener_priority
   type                 = each.value.type
-  vpc_id               = element([for i, j in module.vpc : j.vpc_id], 0)
   app_port_no          = each.value.app_port_no
 #  PROMETHEUS_NODE      = var.PROMETHEUS_NODE
   vpc_cidr             = element([for i, j in module.vpc : j.vpc_cidr], 0)
