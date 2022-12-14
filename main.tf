0c677540bddb6bf618edc14e4d0f3ad58073950b
Mukesh <msahu201989@gmail.com>
2022-12-14 19:28:13 +0530

    listners

--- main.tf
+++ main.tf
@@ -79,7 +79,7 @@ module "apps" {
   private_zone_id      = var.private_zone_id
   public_dns_name      = try(each.value.public_dns_name, null)
   public_zone_id       = var.public_zone_id
-
+  ACM_ARN              = var.ACM_ARN
 }
 
 
@@ -95,7 +95,6 @@ module "alb" {
   vpc_id   = element([for i, j in module.vpc : j.vpc_id], 0)
   vpc_cidr = element([for i, j in module.vpc : j.vpc_cidr], 0)
   internal = each.value.internal
-  ACM_ARN              = var.ACM_ARN
 }
 
 
